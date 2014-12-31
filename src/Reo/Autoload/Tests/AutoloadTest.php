3407b871da5f81622936db86c28b4e0d259d7b9e
Gator92 <schuyler@velargo.com>
2014-12-31 17:42:37 -0500

    update autoloader tests

--- src/Reo/Autoload/Tests/AutoloadTest.php
+++ src/Reo/Autoload/Tests/AutoloadTest.php
@@ -10,52 +10,52 @@ class AutoloaderTest extends PHPUnit_Framework_TestCase
         $this->assertFalse($autoloader->getOption('register_spl'));
         $autoloader->register('Framework\Component\FabStuff', false);
         $this->assertEquals(null, $autoloader->getRegistryEntry('Framework\Component'));
-        $this->assertEquals(false, $autoloader->getRegistryEntry('Framework\Component\FabStuff'));
-        $this->assertEquals(true, $autoloader->checkRegistry('Framework\Component\FabStuff'));
-        $this->assertEquals(false, $autoloader->checkRegistry('Framework\Component'));
+        $this->assertFalse($autoloader->getRegistryEntry('Framework\Component\FabStuff'));
+        $this->assertTrue($autoloader->checkRegistry('Framework\Component\FabStuff'));
+        $this->assertFalse($autoloader->checkRegistry('Framework\Component'));
 
         //root namespace set, still overrides deeper class
         $autoloader->register('Framework', $dir = 'dev/null');
         $this->assertEquals($dir, $autoloader->getRegistryEntry('Framework'));
-        $this->assertEquals(true, $autoloader->checkRegistry('Framework\Component\FabStuff'));
-        $this->assertEquals(false, $autoloader->checkRegistry('Framework'));
+        $this->assertTrue($autoloader->checkRegistry('Framework\Component\FabStuff'));
+        $this->assertFalse($autoloader->checkRegistry('Framework'));
 
         $autoloader = $this->getAutoloader();
         $autoloader->register('Framework\Component', false);
-        $this->assertEquals(true, $autoloader->checkRegistry('Framework\Component\Fabstuff'));
-        $this->assertEquals(true, $autoloader->checkRegistry('Framework\Component\Fabstuff\Blixer'));
-        $this->assertEquals(false, $autoloader->checkRegistry('Framework'));
-        $this->assertEquals(true, $autoloader->checkRegistry('Framework\Component'));
+        $this->assertTrue($autoloader->checkRegistry('Framework\Component\Fabstuff'));
+        $this->assertTrue($autoloader->checkRegistry('Framework\Component\Fabstuff\Blixer'));
+        $this->assertFalse($autoloader->checkRegistry('Framework'));
+        $this->assertTrue($autoloader->checkRegistry('Framework\Component'));
 
         $autoloader = $this->getAutoloader();
         $autoloader->register('Framework', false);
-        $this->assertEquals(true, $autoloader->checkRegistry('Framework\Component\Fabstuff'));
-        $this->assertEquals(true, $autoloader->checkRegistry('Framework\Component\Fabstuff\Blixer'));
-        $this->assertEquals(true, $autoloader->checkRegistry('Framework'));
-        $this->assertEquals(true, $autoloader->checkRegistry('Framework\Component'));
+        $this->assertTrue($autoloader->checkRegistry('Framework\Component\Fabstuff'));
+        $this->assertTrue($autoloader->checkRegistry('Framework\Component\Fabstuff\Blixer'));
+        $this->assertTrue($autoloader->checkRegistry('Framework'));
+        $this->assertTrue($autoloader->checkRegistry('Framework\Component'));
     }
 
     public function testRegistryAutoload()
     {
         $autoloader = $this->getAutoloader();
         $autoloader->register($namespace = 'Framework', $dir = __DIR__ . DIRECTORY_SEPARATOR . 'lib');
-        $this->assertEquals(true, @file_exists($dir));
+        $this->assertTrue(@file_exists($dir));
         $this->assertEquals($dir, $autoloader->getRegistryEntry($namespace));
 
-        $this->assertEquals(false, $autoloader->checkRegistry($namespace . '\Blixer'));
-        $this->assertEquals(true, $autoloader->checkRegistry($class = $namespace . '\Component\Blixer'));
+        $this->assertFalse($autoloader->checkRegistry($namespace . '\Blixer'));
+        $this->assertTrue($autoloader->checkRegistry($class = $namespace . '\Component\Blixer'));
         $this->assertTrue(class_exists($class, false), 'The loaded class does not exists.');
 
         //pear style classes
-        $this->assertEquals(false, $autoloader->checkRegistry($namespace . '_Bloxer'));
-        $this->assertEquals(true, $autoloader->checkRegistry($class = $namespace . '_Component_Bloxer'));
+        $this->assertFalse($autoloader->checkRegistry($namespace . '_Bloxer'));
+        $this->assertTrue($autoloader->checkRegistry($class = $namespace . '_Component_Bloxer'));
         $this->assertTrue(class_exists($class, false), 'The loaded class does not exists.');
     }
 
     public function testLibPathAutoload()
     {
         $autoloader = $this->getAutoloader();
-        $this->assertEquals(true, $autoloader->loadClass('Framework\Component\Bluxer'));
+        $this->assertTrue($autoloader->loadClass('Framework\Component\Bluxer'));
         $this->assertTrue(class_exists('Framework\Component\Bluxer', false), 'The loaded class does not exists.');
         $bluxer = new Framework\Component\Bluxer();
         $this->assertEquals('hello', $bluxer->talk());
